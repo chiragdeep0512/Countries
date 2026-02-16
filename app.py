df33cf62b607ef421503346b51806a1c29cf01ea
Naman Deep Singh <150340237+chiragdeep0512@users.noreply.github.com>
2026-02-17 00:55:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import streamlit as st
+import pkg_resources
+import streamlit as st
+
+installed_packages = [pkg.key for pkg in pkg_resources.working_set]
+st.write("Installed packages:", installed_packages)
 
 st.set_page_config(page_title = 'Country Analysis' , layout='wide')
 st.title('Country Economic Analysis')
 st.write("Multi-page professional Streamlit app.")
 st.write("Use the sidebar to navigate between pages.")
+
 
